d22a7637ceafef38a227e1fa5ecccc5f1712f9dc
kingdayx <54965488+kingdayx@users.noreply.github.com>
2021-06-16 21:05:00 -0400

    changed background

--- src/getWeb3.js
+++ src/getWeb3.js
@@ -1,39 +1,134 @@
 import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 const getWeb3 = () =>
-	new Promise((resolve, reject) => {
-		// Wait for loading completion to avoid race conditions with web3 injection timing.
-		window.addEventListener("load", async () => {
-			// Modern dapp browsers...
-			if (window.ethereum) {
-				const web3 = new Web3(window.ethereum);
-				try {
-					// Request account access if needed
-					await window.ethereum.enable();
-					// Acccounts now exposed
-					resolve(web3);
-				} catch (error) {
-					reject(error);
-				}
-			}
-			// Legacy dapp browsers...
-			else if (window.web3) {
-				// Use Mist/MetaMask's provider.
-				const web3 = window.web3;
-				console.log("Injected web3 detected.");
-				resolve(web3);
-			}
-			// Fallback to localhost; use dev console port by default...
-			else {
-				const web3 = new Web3();
-				const provider = new Web3.providers.HttpProvider(
-					"http://127.0.0.1:7545"
-				);
-				web3.setProvider(provider)
-				console.log("No web3 instance injected, using Local web3.");
-				resolve(web3);
-			}
-		});
-	});
+  new Promise((resolve, reject) => {
+    // Wait for loading completion to avoid race conditions with web3 injection timing.
+    window.addEventListener("load", async () => {
+      // Modern dapp browsers...
+      const provider = await detectEthereumProvider();
+
+      if (provider) {
+        startApp(provider); // Initialize your app
+      } else {
+        console.log("Please install MetaMask!");
+      }
+
+      function startApp(provider) {
+        // If the provider returned by detectEthereumProvider is not the same as
+        // window.ethereum, something is overwriting it, perhaps another wallet.
+        if (provider !== window.ethereum) {
+          console.error("Do you have multiple wallets installed?");
+        }
+        // Access the decentralized web!
+      }
+
+      /**********************************************************/
+      /* Handle chain (network) and chainChanged (per EIP-1193) */
+      /**********************************************************/
+
+      const chainId = await window.ethereum.request({ method: "eth_chainId" });
+      handleChainChanged(chainId);
+
+      window.ethereum.on("chainChanged", handleChainChanged);
+
+      function handleChainChanged(_chainId) {
+        // We recommend reloading the page, unless you must do otherwise
+        window.location.reload();
+      }
+
+      /***********************************************************/
+      /* Handle user accounts and accountsChanged (per EIP-1193) */
+      /***********************************************************/
+
+      let currentAccount = null;
+      window.ethereum
+        .request({ method: "eth_accounts" })
+        .then(handleAccountsChanged)
+        .catch((err) => {
+          // Some unexpected error.
+          // For backwards compatibility reasons, if no accounts are available,
+          // eth_accounts will return an empty array.
+          console.error(err);
+        });
+
+      // Note that this event is emitted on page load.
+      // If the array of accounts is non-empty, you're already
+      // connected.
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
+
+      // For now, 'eth_accounts' will continue to always return an array
+      function handleAccountsChanged(accounts) {
+        if (accounts.length === 0) {
+          // MetaMask is locked or the user has not connected any accounts
+          console.log("Please connect to MetaMask.");
+        } else if (accounts[0] !== currentAccount) {
+          currentAccount = accounts[0];
+          // Do any other work!
+        }
+      }
+
+      /*********************************************/
+      /* Access the user's accounts (per EIP-1102) */
+      /*********************************************/
+
+      // You should only attempt to request the user's accounts in response to user
+      // interaction, such as a button click.
+      // Otherwise, you popup-spam the user like it's 1999.
+      // If you fail to retrieve the user's account(s), you should encourage the user
+      // to initiate the attempt.
+      document.getElementById("connectButton", connect);
+
+      // While you are awaiting the call to eth_requestAccounts, you should disable
+      // any buttons the user can click to initiate the request.
+      // MetaMask will reject any additional requests while the first is still
+      // pending.
+      function connect() {
+        window.ethereum
+          .request({ method: "eth_requestAccounts" })
+          .then(handleAccountsChanged)
+          .catch((err) => {
+            if (err.code === 4001) {
+              // EIP-1193 userRejectedRequest error
+              // If this happens, the user rejected the connection request.
+              console.log("Please connect to MetaMask.");
+            } else {
+              console.error(err);
+            }
+          });
+      }
+
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        try {
+          // Request account access if needed
+          await window.eth_requestAccount();
+          // Acccounts now exposed
+          console.log(web3);
+          resolve(web3);
+        } catch (error) {
+          reject(error);
+        }
+      }
+      // Legacy dapp browsers...
+      else if (window.web3) {
+        // Use Mist/MetaMask's provider.
+        const web3 = window.web3;
+        console.log("Injected web3 detected.");
+        resolve(web3);
+      }
+      // Fallback to localhost; use dev console port by default...
+      else {
+        const web3 = new Web3();
+        const provider = new Web3.providers.HttpProvider(
+          "https://ropsten.infura.io/v3/b33e244c7e2f486bb0252feb96b73b7d"
+        );
+        console.log(provider);
+        web3.setProvider(provider);
+        console.log("No web3 instance injected, using Local web3.");
+        resolve(web3);
+      }
+    });
+  });
 
 export default getWeb3;
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-export const contract = '0x79b4fA2DEFc996E12CfA3b9e7747072091Ae95ad' // ropsten crypto pizza
+export const contract = "0xa612Bda34bE183A89D80D68f57d7e2F4ef772368"; // ropsten crypto pizza
 // export const contract1 = '0xB9528688219Af3cbB8e59696C774feee0630a26D' // stake pool
-export const contract2 = '0x77d672cBf917711cBC2060C9bfDDa6c106B4525B' // otr party token
-export const contract3 = '0xD1aFd30E2C26280e8436de7aee01bdDd74D2Fb1e' // stake pool ropsten
+export const contract2 = "0x08958e30e1f4F6bf0B54E283D88E7eD52647d849"; // otr party token
+export const contract3 = "0x00554E2703C50e78aD2baa74aEb30412DCb60244"; // stake pool ropsten
